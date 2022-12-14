c3f584ab37551a7507f47f08ef0f5a91b1a48141
Piotr ZALEWSKI <piotr.ZALEWSKI@idemia.com>
2022-12-14 07:41:24 +0100

    day14

---
+++ day14/star2.py
@@ -0,0 +1,122 @@
+
+from __future__ import annotations
+
+import argparse
+import os.path
+
+import pytest
+
+import support
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+MAP = {}
+
+
+def print_map():
+    keys = MAP.keys()
+    minx = min([x[0] for x in keys])
+    maxx = max([x[0] for x in keys])
+    miny = min([x[1] for x in keys])
+    maxy = max([x[1] for x in keys])
+    for y in range(miny, maxy+1):
+        print("")
+        for x in range(minx, maxx+1):
+            if (x, y) in keys:
+                print(MAP[(x, y)], end='')
+            else:
+                print(" ", end='')
+    print("")
+
+
+def sand(x, y, maxy):
+    keys = MAP.keys()
+    for b in range(y, maxy+2):
+        y = b
+        # print(f"{(x,b)}")
+        if (x, b+1) not in keys:
+            continue
+        if (x-1, b+1) not in keys:
+            x = x-1
+            continue
+        if (x+1, b+1) not in keys:
+            x = x+1
+            continue
+        if (500, 0) != (x, b):
+            MAP[(x, b)] = "o"
+            return True
+        else:
+            return False
+    MAP[(x, y)] = "o"
+    return True
+
+
+def compute(s: str) -> int:
+    lines = s.splitlines()
+    for line in lines:
+        d = line.split(" ")
+        oldx, oldy = (-1, -1)
+        for a in range(0, len(d), 2):
+            x, y = d[a].split(",")
+            x, y = (int(x), int(y))
+            MAP[(x, y)] = "#"
+            if oldx > 0 and oldy > 0:
+                for a in range(oldx, x+1):
+                    for b in range(oldy, y+1):
+                        MAP[(a, b)] = "#"
+                    for b in range(oldy, y-1, -1):
+                        MAP[(a, b)] = "#"
+                for a in range(oldx, x-1, -1):
+                    for b in range(oldy, y+1):
+                        MAP[(a, b)] = "#"
+                    for b in range(oldy, y-1, -1):
+                        MAP[(a, b)] = "#"
+            oldx = x
+            oldy = y
+    count = 0
+
+    MAP[(500, 0)] = "O"
+    maxy = max([x[1] for x in MAP.keys()])
+
+    while (True):
+        if sand(500, 0, maxy):
+            count += 1
+            # print_map()
+        else:
+            break
+
+    MAP[(500, 0)] = "O"
+    # print_map()
+    return count
+
+
+INPUT_S = '''\
+498,4 -> 498,6 -> 496,6
+503,4 -> 502,4 -> 502,9 -> 494,9
+'''
+EXPECTED = 93
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('data_file', nargs='?', default=INPUT_TXT)
+    args = parser.parse_args()
+
+    with open(args.data_file) as f, support.timing():
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
